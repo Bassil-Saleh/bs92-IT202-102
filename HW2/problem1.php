715b7f6995e8b1f7d1bb0082d2bc84e000732423
Bassil Saleh <salehbassil42@gmail.com>
2024-01-31 20:02:37 -0500

    Added code for printing odd numbers in function processArray() of problem1.php

--- HW2/problem1.php
+++ HW2/problem1.php
@@ -8,6 +8,13 @@ function processArray($arr) {
     echo "<br>Odds output:<br>";
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
     //TODO add logic here to echo out only odd values
+
+    // Solution to Problem 1:
+    foreach($arr as $a) {
+        if($a % 2 == 1) {
+            echo $a;
+        }
+    }
 }
 echo "Problem 1: Odd Output<br>";
 ?>
