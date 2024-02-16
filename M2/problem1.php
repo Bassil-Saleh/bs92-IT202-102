4e647d9488e53c7e34322067e20749c27c08e8e6
Bassil Saleh <salehbassil42@gmail.com>
2024-02-16 15:52:38 -0500

    Added code to output odd values in problem1.php

--- M2/problem1.php
+++ M2/problem1.php
@@ -8,7 +8,15 @@ function processArray($arr) {
     echo "<br>Odds output:<br>";
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
     //TODO add logic here to echo out only odd values
-
+    
+    // Name: Bassil Saleh
+    // UCID: bs92
+    // Date: 2-16-2024
+    foreach($arr as $i) {
+        if($i % 2 == 1) {
+            echo $i . "<br>";
+        }
+    } 
 }
 echo "Problem 1: Odd Output<br>";
 ?>
