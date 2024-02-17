1284ecd6e8168a64b426ad7a94e3999d3860298a
Bassil Saleh <salehbassil42@gmail.com>
2024-02-16 20:38:04 -0500

    Added code for problem3.php

--- M2/problem3.php
+++ M2/problem3.php
@@ -9,6 +9,36 @@ function bePositive($arr) {
     echo "<br>Positive output:<br>";
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO use echo to output all of the values as positive (even if they were originally positive) and maintain the original datatype
+
+    // Name: Bassil Saleh
+    // UCID: bs92
+    // Date: 2-16-2024
+    foreach($arr as $i) {
+        
+        if((gettype($i) == "string") && ($i[0] == "-")) {
+            // The order of these Boolean expressions is important.
+            // If the element turns out to not be a string,  
+            // then the second expression (and in turn this 
+            // code block) will not execute.
+            // This is because of short circuit evaluation;
+            // if the first term in a Boolean AND expression
+            // is false, then the entire expression will be 
+            // false no matter what the other terms are.
+            // This prevents PHP from attempting to extract 
+            // a character from a non-string datatype.
+            $i = str_replace("-","",$i);
+        }
+        elseif($i < 0) {
+            // Multiplying a string containing a number
+            // implicitly converts the final result 
+            // to a number, so I put more than 
+            // one conditional block to prevent this.
+            $i *= -1;
+        }
+        //echo var_dump($i) . "<br>";   // For checking if the data type was preserved
+        echo "$i<br>";
+    }
+
     //hint: may want to use var_dump() or similar to show final data types
 }
 echo "Problem 3: Be Positive<br>";
