8d1c25ab1875611dae775ce217dd1e9e052906d7
Bassil Saleh <salehbassil42@gmail.com>
2024-02-21 19:08:15 -0500

    profile.php only outputs email; added Profile link to navigation bar

--- M1/profile.php
+++ M1/profile.php
@@ -6,7 +6,7 @@ require(__DIR__ . "/partials/nav.php");
 echo "<h2>Profile Page</h2>";
 
 if (is_logged_in()) {
-    echo "Welcome, " . get_user_email();
+    echo $_SESSION["user"]["email"];
 } else {
     echo "You're not logged in";
 }
