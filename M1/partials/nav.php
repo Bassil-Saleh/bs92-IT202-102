98ea57061bdea77354b53d617032fb318d1937c7
Bassil Saleh <salehbassil42@gmail.com>
2024-03-15 13:38:55 -0400

    Create Roles renamed to Create Role in navigation bar

--- M1/partials/nav.php
+++ M1/partials/nav.php
@@ -55,7 +55,7 @@ require_once(__DIR__ . "/../lib/functions.php");
             <li><a href="home.php">Home</a></li>
             <li><a href="profile.php">Profile</a></li>
             <li><a href="list_roles.php">List Roles</a></li>
-            <li><a href="create_role.php">Create Roles</a></li>
+            <li><a href="create_role.php">Create Role</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="login.php">Login</a></li>
