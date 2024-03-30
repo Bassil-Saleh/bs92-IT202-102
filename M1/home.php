625b42030ee73930b66baa4cded3cdab0e8cc577
Bassil Saleh <salehbassil42@gmail.com>
2024-03-29 21:08:47 -0400

    home.php will not attempt to read from ['user']['roles'] unless is_logged_in() returns True

--- M1/home.php
+++ M1/home.php
@@ -6,24 +6,25 @@ displayFlashMessages();
 <?php
 if (is_logged_in()) {
     echo "<div id=\"homepage_welcome\">Welcome, " . get_user_email() . "!</div>";
+    // Check if logged in user has administrator privileges
+    $role_array = $_SESSION['user']['roles'];
+    if($role_array) {
+        // non-empty array; check for role 'Admin'
+        if($role_array[0]['name'] == 'Admin') {
+            echo "<div class=\"user_role_msg\">Your account has administrator privileges.</div>";
+        }
+        else {
+            echo "<div class=\"user_role_msg\">Your account does not have administrator privileges.</div>";
+        }
+    }
+    else {
+        // empty array
+        echo "<div class=\"user_role_msg\">Your account does not have administrator privileges.</div>";
+    }
 } else {
     echo "<div id=\"homepage_logged_out\">You're not logged in.</div>";
 }
 //shows session info
 //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
-// Check if logged in user has administrator privileges
-$role_array = $_SESSION['user']['roles'];
-if($role_array) {
-    // non-empty array; check for role 'Admin'
-    if($role_array[0]['name'] == 'Admin') {
-        echo "<div class=\"user_role_msg\">Your account has administrator privileges.</div>";
-    }
-    else {
-        echo "<div class=\"user_role_msg\">Your account does not have administrator privileges.</div>";
-    }
-}
-else {
-    // empty array
-    echo "<div class=\"user_role_msg\">Your account does not have administrator privileges.</div>";
-}
+
 ?>
