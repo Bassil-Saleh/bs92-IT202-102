4a9757b5d1a636fbfdb4bc3e6cb9010b34c4ad65
Bassil Saleh <salehbassil42@gmail.com>
2024-04-13 20:09:19 -0400

    Added SQL queries for creating a system user and a world account for the system user; foreign key in Accounts table now references Users table

--- M2/sql/make_accounts_table.sql
+++ M2/sql/make_accounts_table.sql
@@ -1,7 +1,7 @@
 -- Active: 1707348080693@@db.ethereallab.app@3306@bs92
 CREATE TABLE IF NOT EXISTS `Accounts` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `account_number` DECIMAL(12,0),
+    `account_number` CHAR(12) DEFAULT NULL,
     `user_id` INT DEFAULT NULL,
     `balance` INT NOT NULL DEFAULT 0,
     `account_type` VARCHAR(255) DEFAULT NULL,
@@ -9,5 +9,5 @@ CREATE TABLE IF NOT EXISTS `Accounts` (
     `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE KEY (`account_number`),
-    FOREIGN KEY (`user_id`) REFERENCES `UserRoles` (`user_id`)
-)
+    FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`)
+);
