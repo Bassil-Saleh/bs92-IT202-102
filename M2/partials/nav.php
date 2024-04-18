5fd5ac0d38167a0d74a068cd127d481cc45f8cd6
Bassil Saleh <salehbassil42@gmail.com>
2024-04-18 12:43:34 -0400

    List Roles and Create Role pages are visible if the logged in user has the Admin role

--- M2/partials/nav.php
+++ M2/partials/nav.php
@@ -192,6 +192,8 @@ require_once(__DIR__ . "/../lib/functions.php");
         <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
             <li><a href="profile.php">Profile</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in() && has_role("Admin")) : ?>
             <li><a href="list_roles.php">List Roles</a></li>
             <li><a href="create_role.php">Create Role</a></li>
         <?php endif; ?>
