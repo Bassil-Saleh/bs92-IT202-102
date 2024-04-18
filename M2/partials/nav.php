ab8e6d65e0b8bd5a354b8548dacfadfcf96558dc
Bassil Saleh <salehbassil42@gmail.com>
2024-04-18 12:51:35 -0400

    Added dashboard.php; Navigation bar now includes Dashboard link

--- M2/partials/nav.php
+++ M2/partials/nav.php
@@ -192,6 +192,7 @@ require_once(__DIR__ . "/../lib/functions.php");
         <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
             <li><a href="profile.php">Profile</a></li>
+            <li><a href="dashboard.php">Dashboard</a></li>
         <?php endif; ?>
         <?php if (is_logged_in() && has_role("Admin")) : ?>
             <li><a href="list_roles.php">List Roles</a></li>
