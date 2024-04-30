d1ccdae4fe010a519eaca7fbb785a519954d86d4
Bassil Saleh <salehbassil42@gmail.com>
2024-04-30 17:06:00 -0400

    Populate HTML table with records from Transaction table associated with clicked account #

--- M2/transaction_history.php
+++ M2/transaction_history.php
@@ -15,7 +15,7 @@ if (isset($_POST)) {
         $stmt->execute(['account_number' => $account_num]);
         $result = $stmt->fetch();
         $id = $result['id'];
-        echo "$account_num $id";
+        //echo "$account_num $id";
         // Get all records from the Transactions table where 
         // account_src == clicked account # or account_dest == clicked account #
         $stmt = $db->prepare("SELECT `account_src`, `account_dest`, `transaction_type`, `balance_change`, `created`, `expected_total`, `memo` FROM `Transactions` WHERE (`account_src` = :account_id) OR (`account_dest` = :account_id)");
@@ -23,7 +23,7 @@ if (isset($_POST)) {
         $result = $stmt->fetch();
         if ($result) {
             $records = $stmt->fetchALL(PDO::FETCH_ASSOC);
-            echo var_dump($records);
+            //echo var_dump($records);
         }
     } catch (Exception $e) {
         echo var_dump($e);
@@ -42,5 +42,24 @@ if (isset($_POST)) {
             <th>Memo</th>
         </tr>
     </thead>
+    <tbody>
+        <?php if (empty($records)):?>
+            <tr>
+                <td colspan="%100">No transaction(s) found on this account.</td>
+            </tr>
+        <?php else:?>
+            <?php foreach($records as $transaction):?>
+                <tr>
+                    <td><?php se($transaction['account_src']);?></td>
+                    <td><?php se($transaction['account_dest']);?></td>
+                    <td><?php se($transaction['transaction_type']);?></td>
+                    <td><?php se($transaction['balance_change']);?></td>
+                    <td><?php se($transaction['created']);?></td>
+                    <td><?php se($transaction['expected_total']);?></td>
+                    <td><?php se($transaction['memo']);?></td>
+                </tr>
+            <?php endforeach;?>
+        <?php endif;?>
+    </tbody>
 </table>
 <div id="transaction_history_go_back"><a href="accounts.php">Back to My Accounts</a></div>
