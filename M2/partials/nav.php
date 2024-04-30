a0241ac45cd3c98747963b0185301ebde95cf43a
Bassil Saleh <salehbassil42@gmail.com>
2024-04-30 19:26:46 -0400

    <style> element  moved below session_start()

--- M2/partials/nav.php
+++ M2/partials/nav.php
@@ -1,3 +1,28 @@
+<?php
+session_start();
+//Note: this is to resolve cookie issues with port numbers
+$domain = $_SERVER["HTTP_HOST"];
+if (strpos($domain, ":")) {
+    $domain = explode(":", $domain)[0];
+}
+$localWorks = true; //some people have issues with localhost for the cookie params
+//if you're one of those people make this false
+
+//this is an extra condition added to "resolve" the localhost issue for the session cookie
+/*if (($localWorks && $domain == "localhost") || $domain != "localhost") {
+    session_set_cookie_params([
+        "lifetime" => 60 * 60,
+        "path" => "/Project",
+        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
+        "domain" => $domain,
+        "secure" => true,
+        "httponly" => true,
+        "samesite" => "lax"
+    ]);
+}*/
+require_once(__DIR__ . "/../lib/functions.php");
+
+?>
 <style>
 /* Navigation bar styling: */
 nav.navigation_bar ul {
@@ -218,31 +243,6 @@ select {
   margin: 8px 24px;
 }
 </style>
-<?php
-session_start();
-//Note: this is to resolve cookie issues with port numbers
-$domain = $_SERVER["HTTP_HOST"];
-if (strpos($domain, ":")) {
-    $domain = explode(":", $domain)[0];
-}
-$localWorks = true; //some people have issues with localhost for the cookie params
-//if you're one of those people make this false
-
-//this is an extra condition added to "resolve" the localhost issue for the session cookie
-/*if (($localWorks && $domain == "localhost") || $domain != "localhost") {
-    session_set_cookie_params([
-        "lifetime" => 60 * 60,
-        "path" => "/Project",
-        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
-        "domain" => $domain,
-        "secure" => true,
-        "httponly" => true,
-        "samesite" => "lax"
-    ]);
-}*/
-require_once(__DIR__ . "/../lib/functions.php");
-
-?>
 <nav class="navigation_bar">
     <ul>
         <?php if (is_logged_in()) : ?>
