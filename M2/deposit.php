3f470a4795f6b2bfcb62a7bc04c3c1299b2d709f
Bassil Saleh <salehbassil42@gmail.com>
2024-05-01 21:11:48 -0400

    Deposit or withdraw option is required

--- M2/deposit.php
+++ M2/deposit.php
@@ -3,7 +3,12 @@ require(__DIR__ . "/partials/nav.php");
 ?>
 <?php
 if (isset($_POST) && !empty($_POST)) {
-    echo var_dump($_POST);
+    // echo var_dump($_POST);
+    $option = $_POST['select_deposit_or_withdraw'];
+    $account_number = $_POST['account_numbers'];
+    $amount_of_money = $_POST['amount_of_money'];
+    echo "$option $account_number $amount_of_money";
+
 }
 ?>
 <?php
@@ -29,24 +34,24 @@ try {
     echo var_dump($e);
 }
 ?>
-<h1 class="page_name_header">Deposit and Withdraw</h1>
+<h1 class="page_name_header">Deposit or Withdraw</h1>
 <?php if(empty($records)):?>
     <h3 class="page_name_header">No account number(s) found under your name.</h3>
 <?php else:?>
 <h3 class="page_name_header">I want to:</h3>
 <form id="deposit_or_withdraw" class="binary_selection" action="deposit.php" method="POST">
-    <input class="radio_option" type="radio" id="deposit" name="select_deposit_or_withdraw" value="deposit">
+    <input class="radio_option" type="radio" id="deposit" name="select_deposit_or_withdraw" value="deposit" required>
     <label for="deposit">Make a deposit</label>
-    <input class="radio_option" type="radio" id="withdraw" name="select_deposit_or_withdraw" value="withdraw">
+    <input class="radio_option" type="radio" id="withdraw" name="select_deposit_or_withdraw" value="withdraw" required>
     <label for="withdraw">Make a withdrawal</label>
-    <label for="account_numbers"><h3 class="page_name_header">Deposit to/Withdraw from Account #:</h3></label>
+    <label for="account_numbers"><h3 class="page_name_header">Account #:</h3></label>
     <select name="account_numbers" id="account_numbers" class="dropdown_menu">
         <!-- Populate this dropdown menu with accounts associated with the logged in user -->
         <?php foreach($records as $account):?>
             <option value="<?php se($account['account_number']);?>"><?php se($account['account_number']);?></option>
         <?php endforeach;?>
     </select>
-    <label for="amount_of_money"><h3 class="page_name_header">Deposit/Withdrawal Amount:</h3></label>
+    <label for="amount_of_money"><h3 class="page_name_header">Amount ($):</h3></label>
     <input id="amount_of_money" name="amount_of_money" class="one_line_field" type="number" value="0.01" min="0.01" step="0.01" required>
     <input class="submit_button" type=submit value="Submit">
 </form>
